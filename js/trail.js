1beff23545b2cc79245c1b39335a588fb6b7516e
SGezha <dr.uma228@gmail.com>
2019-11-27 09:32:58 +0300

    smooth trail ✔👍

--- js/trail.js
+++ js/trail.js
@@ -1,60 +1,82 @@
-function StartMouseTrail() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-
-    // Get proper height and width for canvas, then set resize handler.
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    window.addEventListener(
-        "resize",
-        ({ target: { innerWidth, innerHeight } }) => {
-            canvas.width = innerWidth;
-            canvas.height = innerHeight;
-        },
-        false
-    );
-
-    const startAnimation = () => {
-        // Use a closure here to keep internal state.
-        let lastX = 0;
-        let lastY = 0;
-        let currX = 0;
-        let currY = 0;
-
-        const update = () => {
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.moveTo(lastX, lastY);
-            ctx.lineTo(currX, currY);
-            ctx.strokeStyle = "#34e89e";
-            ctx.stroke();
-
-            lastX = currX;
-            lastY = currY;
-
-            // Fade out the previous tails
-            ctx.fillStyle = `rgba(0, 0, 0, 0.1)`;
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-            // Request an animation frame to update it for a smooth 60fps independent of mousemove updates.
-            requestAnimationFrame(update);
-        };
-
-        // On mouse move update cursor position.
-        window.addEventListener(
-            "mousemove",
-            ({ pageX, pageY }) => {
-                currX = pageX;
-                currY = pageY;
-            },
-            false
-        );
-
-        // Start the update cycle.
-        update();
-    };
-
-    startAnimation();
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.lifetime = 0;
+    }
 }
 
-StartMouseTrail()
+window.addEventListener(
+    'resize',
+    () => {
+        this.setState({
+            cHeight: document.body.clientHeight,
+            cWidth: document.body.clientWidth,
+        });
+    },
+    false,
+);
+
+startAnimation = () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const points = [];
+
+    const addPoint = (x, y) => {
+        const point = new Point(x, y);
+        points.push(point);
+    };
+
+    document.addEventListener('mousemove', ({
+        clientX,
+        clientY
+    }) => {
+        addPoint(clientX - canvas.offsetLeft, clientY - canvas.offsetTop);
+    }, false);
+
+    const animatePoints = () => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        const duration = 0.5 * (1 * 1000) / 100;
+
+        for (let i = 0; i < points.length; ++i) {
+            const point = points[i];
+            let lastPoint;
+
+            if (points[i - 1] !== undefined) {
+                lastPoint = points[i - 1];
+            } else lastPoint = point;
+
+            point.lifetime += 0.2;
+
+            if (point.lifetime > duration) {
+                points.shift();
+            } else {
+                const lifePercent = (point.lifetime / duration);
+                const spreadRate = 2 * (1 - lifePercent);
+
+                ctx.lineJoin = 'round';
+                ctx.lineWidth = spreadRate;
+
+                const red = Math.floor(50 - (50 * lifePercent));
+                const green = Math.floor(200 + (200 * lifePercent));
+                const blue = 158;
+                ctx.strokeStyle = `rgb(${red},${green},${blue}`;
+
+                ctx.beginPath();
+
+                ctx.moveTo(lastPoint.x, lastPoint.y);
+                ctx.lineTo(point.x, point.y);
+
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+        requestAnimationFrame(animatePoints);
+    };
+    animatePoints();
+}
+
+window.onload = function () {
+    startAnimation();
+}
